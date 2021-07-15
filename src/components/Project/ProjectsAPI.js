035f1489825ced9f428eababd3d4ace0c056eea1
ela-ocana-jalafund <Ela.Ocana@fundacion-jala.org>
2021-07-15 18:50:38 -0400

    Delete bugs

--- src/components/Project/ProjectsAPI.js
+++ src/components/Project/ProjectsAPI.js
@@ -12,29 +12,3 @@ export default async function GetProjects() {
     console.log(error);
   }
 }
-
-/*const projects = [
-  {
-    id: 1,
-    name: "Coffe Shop",
-    description: "Project to manage coffe shop resource",
-    contact: "luis@fundacion-jala",
-    creationDate: "14/06/2021",
-  },
-  {
-    id: 2,
-    name: "Form's",
-    description: "Project Cafeteria",
-    contact: "rocio@fundacion-jala",
-    creationDate: "10/06/2021",
-  },
-  {
-    id: 3,
-    name: "TASK SHOP",
-    description: "Project to manage tasks",
-    contact: "rocio@fundacion-jala",
-    creationDate: "05/06/2021",
-  },
-];
-
-export default projects;*/
