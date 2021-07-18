e893692ae374e1aeb703d36c23b268f7e8e465c1
ela-ocana-jalafund <Ela.Ocana@fundacion-jala.org>
2021-07-18 19:30:08 -0400

    Delete comments and add test with jasonplaceholder

--- src/components/Project/Card.jsx
+++ src/components/Project/Card.jsx
@@ -1,15 +1,24 @@
-//import { Email, CalendarToday, Description } from "@material-ui/icons";
+import { Description, Email, CalendarToday } from "@material-ui/icons";
 import "./Card.scss";
 
 function Card(props) {
-  const { id, name } = props;
-  //const { id, name, description, contact, creationDate } = props;
+  const { id, name, username, email, address } = props;
   return (
-    <>
-      <div key={id} className="card">
-        {name}
+    <div key={id} className="Card-Container">
+      <div className="Name">{name}</div>
+      <div className="Field Description">
+        <Description color="primary" />
+        <label>{username}</label>
       </div>
-    </>
+      <div className="Field Contact">
+        <Email color="primary" />
+        <label>{email}</label>
+      </div>
+      <div className="Field Creation-date">
+        <CalendarToday color="primary" />
+        <label>{address.zipcode}</label>
+      </div>
+    </div>
   );
 }
 
