08abe0f3aa15efce680510808d44aa5cae7c5d19
Casfer98 <alejandrocastellonfer@gmail.com>
2021-07-20 20:07:50 -0400

    remove index changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-// import App from "./components/App/App";
+import App from "./components/App/App";
 import React from "react";
 import ReactDOM from "react-dom";
-import Resume from "./components/Resume/ResumeDetail/ResumeDetail";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Resume resumeId="https://jsonplaceholder.typicode.com/users/1"></Resume>
+    <App></App>
   </React.StrictMode>,
   document.getElementById("root")
 );
