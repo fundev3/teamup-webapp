56ae21d7498aeb3ac0f7d83887be62e42ea784a8
lilian-delgado-jalafund <Lilian.Delgado@fundacion-jala.org>
2021-07-22 14:30:38 -0400

    AB#127 Delete unnecessary divs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,32 +6,30 @@ import "./App.scss";
 
 function App() {
   return (
-    <>
-      <Router>
-        <NavBar></NavBar>
-        <div className="content">
-          <Switch>
-            <Route component={Home} exact path="/"></Route>
-            <Route exact path="/projects">
-              <Typography
-                style={{ color: "#9E9EAA", fontWeight: "700" }}
-                variant="h4"
-              >
-                Projects coming soon!
-              </Typography>
-            </Route>
-            <Route exact path="/resumes">
-              <Typography
-                style={{ color: "#9E9EAA", fontWeight: "700" }}
-                variant="h4"
-              >
-                Resumes coming soon!
-              </Typography>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </>
+    <Router>
+      <NavBar />
+      <div className="content">
+        <Switch>
+          <Route component={Home} exact path="/"></Route>
+          <Route exact path="/projects">
+            <Typography
+              style={{ color: "#9E9EAA", fontWeight: "700" }}
+              variant="h4"
+            >
+              Projects coming soon!
+            </Typography>
+          </Route>
+          <Route exact path="/resumes">
+            <Typography
+              style={{ color: "#9E9EAA", fontWeight: "700" }}
+              variant="h4"
+            >
+              Resumes coming soon!
+            </Typography>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/App/NavBar.jsx
+++ src/components/App/NavBar.jsx
@@ -1,67 +1,63 @@
-import AppBar from "@material-ui/core/AppBar";
 import { NavLink } from "react-router-dom";
 import React from "react";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import "./NavBar.scss";
 
 function NavBar() {
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar className="navbar">
-          <div className="logo-content">
-            <Typography
-              style={{
-                fontSize: "1.7rem",
-                fontWeight: "700",
-                letterSpacing: "-0.08em",
-              }}
-            >
-              TeamUp
-            </Typography>
-          </div>
-          <div className="navbar-content">
-            <NavLink
-              activeStyle={{
-                color: "#64ffda",
-                fontWeight: "bold",
-              }}
-              className="navbar-label"
-              exact
-              style={{ color: "#ffffff", textDecoration: "none" }}
-              to="/"
-            >
-              <Typography variant="h6">Home</Typography>
-            </NavLink>
-            <NavLink
-              activeStyle={{
-                color: "#64ffda",
-                fontWeight: "bold",
-              }}
-              className="navbar-label"
-              exact
-              style={{ color: "#ffffff", textDecoration: "none" }}
-              to="/projects"
-            >
-              <Typography variant="h6">Projects</Typography>
-            </NavLink>
-            <NavLink
-              activeStyle={{
-                color: "#64ffda",
-                fontWeight: "bold",
-              }}
-              className="navbar-label"
-              exact
-              style={{ color: "#ffffff", textDecoration: "none" }}
-              to="/resumes"
-            >
-              <Typography variant="h6">Resumes</Typography>
-            </NavLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position="static">
+      <Toolbar className="navbar">
+        <div className="logo-content">
+          <Typography
+            style={{
+              fontSize: "1.7rem",
+              fontWeight: "700",
+              letterSpacing: "-0.08em",
+            }}
+          >
+            TeamUp
+          </Typography>
+        </div>
+        <div className="navbar-content">
+          <NavLink
+            activeStyle={{
+              color: "#64ffda",
+              fontWeight: "bold",
+            }}
+            className="navbar-label"
+            exact
+            style={{ color: "#ffffff", textDecoration: "none" }}
+            to="/"
+          >
+            <Typography variant="h6">Home</Typography>
+          </NavLink>
+          <NavLink
+            activeStyle={{
+              color: "#64ffda",
+              fontWeight: "bold",
+            }}
+            className="navbar-label"
+            exact
+            style={{ color: "#ffffff", textDecoration: "none" }}
+            to="/projects"
+          >
+            <Typography variant="h6">Projects</Typography>
+          </NavLink>
+          <NavLink
+            activeStyle={{
+              color: "#64ffda",
+              fontWeight: "bold",
+            }}
+            className="navbar-label"
+            exact
+            style={{ color: "#ffffff", textDecoration: "none" }}
+            to="/resumes"
+          >
+            <Typography variant="h6">Resumes</Typography>
+          </NavLink>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 }
 
