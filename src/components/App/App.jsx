d59e1b47d61d432f36b4ebeeb5dc212462e58abb
lilian-delgado-jalafund <Lilian.Delgado@fundacion-jala.org>
2021-07-21 22:52:25 -0400

    AB#127: Add NavBar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,38 @@
-import ProjectForm from "../ProjectForm/ProjectForm";
-
+import { Home } from "./Home.jsx";
+import NavBar from "./NavBar";
+import { Typography } from "@material-ui/core";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.scss";
 
 function App() {
-  return <ProjectForm />;
+  return (
+    <>
+      <Router>
+        <NavBar></NavBar>
+        <div className="content">
+          <Switch>
+            <Route component={Home} exact path="/"></Route>
+            <Route exact path="/projects">
+              <Typography
+                style={{ color: "#9E9EAA", fontWeight: "700" }}
+                variant="h4"
+              >
+                Projects coming soon!
+              </Typography>
+            </Route>
+            <Route exact path="/resumes">
+              <Typography
+                style={{ color: "#9E9EAA", fontWeight: "700" }}
+                variant="h4"
+              >
+                Resumes coming soon!
+              </Typography>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
