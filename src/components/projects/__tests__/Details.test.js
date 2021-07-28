e5951cabb67da3a352ab3ac9d652d2c2125cd2fe
Rodrigo Baldiviezo <rodrigo.baldiviezo@fundacion-jala.org>
2021-07-27 21:06:17 -0400

    Add Unit Tests

--- src/components/projects/__tests__/Details.test.js
+++ src/components/projects/__tests__/Details.test.js
@@ -1,41 +1,67 @@
-import { BrowserRouter } from "react-router-dom";
 import Details from "../Details";
+import axios from "axios";
+import { getProject } from "../ProjectsAPI";
+import { MemoryRouter, Route } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-test("should render loading in Details component", () => {
+jest.mock("axios");
+
+const mockData = {
+  data: {
+    contact: {
+      idResume: "5a7939fd-59de-44bd-a092-f5d8434584de",
+      name: "Jose Ecos",
+    },
+    creationDate: "2021-07-18T00:00:00+00:00",
+    description: "Centralize resumes and project",
+    id: "5a7939fd-59de-44bd-a092-f5d8434584de",
+    logo: "https://www.example.com/images/dinosaur.jpg",
+    memberList: [
+      {
+        idResume: "536316e6-f8f6-41ea-b1ce-455b92be9303",
+        name: "Paola Quintanilla",
+      },
+    ],
+    name: "TeamUp",
+    state: true,
+    textInvitation: "You are invited to be part of TeamUp",
+  },
+};
+
+const mockId = "5a7939fd-59de-44bd-a092-f5d8434584de";
+
+const url = `https://fa-tuapi-projects-dev-bra.azurewebsites.net/api/v1/projects/${mockId}`;
+
+test("should return successfully data from an API", async () => {
+  axios.get.mockImplementationOnce(() => Promise.resolve(mockData));
+
+  await expect(getProject(mockId)).resolves.toEqual(mockData.data);
+});
+
+test("should return erroneously data from API", async () => {
+  const handlerError = true;
+  axios.get.mockImplementationOnce(() => Promise.reject(new Error("Fail")));
+
+  const jsdomAlert = window.alert;
+  window.alert = () => {};
+
+  const result = await getProject(mockId);
+  expect(result).toEqual(handlerError);
+
+  window.alert = jsdomAlert;
+});
+
+test("should return Details Component", () => {
+  axios.get.mockImplementationOnce(() => Promise.resolve(mockData));
+
   render(
-    <BrowserRouter>
-      <Details />
-    </BrowserRouter>
-  );
-  const loading = screen.getByText("Loading");
-  expect(loading).toBeInTheDocument();
-});
-
-/* beforeAll(() => {
-  jest.useFakeTimers();
-});
-
-afterAll(() => {
-  jest.useRealTimers();
-});
-
-test("should render Project Details component", async () => {
-  jest.mock("react-router-dom", () => ({
-    ...jest.requireActual("react-router-dom"),
-    useParams: () => ({
-      id: "5a7939fd-59de-44bd-a092-f5d8434584de",
-    }),
-    useRouteMatch: () => ({ url: "/projects/id" }),
-  }));
-  render(
-    <BrowserRouter>
-      <Details />
-    </BrowserRouter>
+    <MemoryRouter initialEntries={[`projects/${mockId}`]}>
+      <Route path="projects/:id">
+        <Details />
+      </Route>
+    </MemoryRouter>
   );
 
-  expect(
-    await screen.findByText("Project: TeamUp", {}, { timeout: 3000 })
-  ).toBeInTheDocument();
-}); */
+  expect(axios.get).toHaveBeenCalledWith(url);
+});
