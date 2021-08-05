e4b01fc2653ccd5ae2d4e07481787dc033a16554
Lilian <85975914+lilian-delgado-jalafund@users.noreply.github.com>
2021-08-04 21:34:33 -0400

    Feature 225 / UI refactor for project list (#44)
    * UI refactor for project list
    
    * Add makeStyles and change error message

--- src/components/projects/Card.jsx
+++ src/components/projects/Card.jsx
@@ -1,26 +1,63 @@
-import { CalendarToday, Person } from "@material-ui/icons";
+import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import { Typography } from "@material-ui/core";
+import image from "../../assets/project-img.jpeg";
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
 import "./Card.scss";
 
+const useStyles = makeStyles({
+  accountIcon: {
+    color: "#e2e2e2",
+    fontSize: "1.8rem",
+  },
+  descriptionText: {
+    fontSize: "0.9rem",
+    textAlign: "justify",
+  },
+});
 function Card(props) {
+  const classes = useStyles();
   const { id, contact, creationDate, description, name } = props;
-  const creationDateFormatted = creationDate.split("T")[0];
+  const creationDateFormatted = creationDate
+    .split("T")[0]
+    .split("-")
+    .reverse()
+    .join("/");
+
+  const history = useHistory();
+
+  function handleViewClick(id) {
+    history.push(`/projects/${id}`);
+  }
 
   return (
-    <div className="Card-Container" key={id}>
-      <div className="name">{name}</div>
-      <div className="description">
-        <label>{description}</label>
+    <div className="card-container" key={id}>
+      <div className="card-info">
+        <div className="left">
+          <img alt="logo" src={image} />
+        </div>
+        <div className="right">
+          <div className="name">{name}</div>
+          <label className="creation-date">
+            Created: {creationDateFormatted}
+          </label>
+          <div className="description">
+            <Typography className={classes.descriptionText}>
+              {description}
+            </Typography>
+          </div>
+        </div>
       </div>
-      <div className="detail-field">
-        <Person color="primary" style={{ fontSize: "1.3rem" }} />
-        <label className="details">{contact.name}</label>
-      </div>
-      <div className="detail-field">
-        <CalendarToday
-          color="primary"
-          style={{ fontSize: "1.2rem", marginLeft: "1px" }}
+      <div className="card-bottom">
+        <div className="detail-field">
+          <AccountCircleRoundedIcon className={classes.accountIcon} />
+          <label className="details">{contact.name}</label>
+        </div>
+        <ArrowForwardIosIcon
+          className="arrow"
+          onClick={() => handleViewClick(id)}
         />
-        <label className="details">{creationDateFormatted}</label>
       </div>
     </div>
   );
