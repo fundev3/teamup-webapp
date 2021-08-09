8840d5fc555f2d74dfbceebdacc14b794da3e85d
Rodrigo Baldiviezo <rodrigo.baldiviezo@fundacion-jala.org>
2021-08-09 19:42:54 -0400

    Provide store application

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,22 @@
+import AlertService from "../../helpers/AlertService";
 import { Home } from "./Home.jsx";
 import NavBar from "./NavBar";
 import ProjectDetails from "../projects/Details";
 import ProjectEntry from "../projects/Entry";
 import Projects from "../projects/Projects";
-import React from "react";
 import ResumeDetails from "../resumes/Details";
 import ResumeEntry from "../resumes/Entry";
 import Resumes from "../resumes/Resumes";
+import { selectAlert } from "../../store/selectors.js";
+import { useSelector } from "react-redux";
+import React, { useState } from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.scss";
 
 function App() {
+  const alert = useSelector(selectAlert);
+  const [open, setOpen] = useState(alert.showAlert);
+
   return (
     <Router>
       <NavBar />
@@ -30,6 +36,12 @@ function App() {
           <Route component={ResumeDetails} exact path="/resumes/:id" />
         </Switch>
       </div>
+      <AlertService
+        message={alert.message}
+        open={open}
+        setOpen={setOpen}
+        type={alert.type}
+      />
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import App from "./components/App/App";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
+import store from "./store";
 import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App></App>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App></App>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
