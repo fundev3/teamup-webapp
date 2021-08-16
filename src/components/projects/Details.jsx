aaf187dcdbf84817c03453e5c725ab80dd1cb67f
Rodrigo Baldiviezo <rodrigo.baldiviezo@fundacion-jala.org>
2021-08-16 18:35:58 -0400

    Add base layout and back button

--- src/components/projects/Details.jsx
+++ src/components/projects/Details.jsx
@@ -1,15 +1,20 @@
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import ArrowBackIos from "@material-ui/icons/ArrowBackIos";
 import { getProject } from "./ProjectsAPI";
+import image from "../../assets/img_avatar.jpg";
 import { isEmpty } from "../../helpers";
+import logo2 from "../../assets/project-img.jpeg";
 import {
+  Avatar,
   Button,
   Container,
   Divider,
   Grid,
+  Paper,
   Typography,
 } from "@material-ui/core";
 import { Link, useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
+import "./Details.scss";
 
 function Details() {
   const { id } = useParams();
@@ -30,54 +35,45 @@ function Details() {
 
   return (
     <div>
-      <Container maxWidth="sm">
-        <Link to="/projects">
-          <Button color="primary" variant="contained">
-            <ArrowBackIcon></ArrowBackIcon>
-          </Button>
+      <Container className="container">
+        <Link className="back-button" to="/projects">
+          <ArrowBackIos></ArrowBackIos>
+          Back
         </Link>
-        <Grid
-          alignContent="center"
-          alignItems="center"
-          container
-          direction="row"
-          justifyContent="center"
-          spacing={1}
-          wrap="nowrap"
-        >
-          <Grid item xs={2}>
-            <img alt="" height="75" src={logo} width="75" />
-          </Grid>
-          <Grid item xs={10}>
-            <Typography align="justify" gutterBottom variant="h4">
-              Project: {name}
+        <Paper className="paper" elevation={3}>
+          <div className="header">
+            <img alt="" src={logo2} width="140" />
+            <Container className="info">
+              <Typography align="justify" gutterBottom variant="h5">
+                {name}
+              </Typography>
+              <Typography color="textSecondary">
+                Created: {creationDate}
+              </Typography>
+              <div className="owner">
+                <Avatar className="avatar" src={image} />
+                <Typography align="justify" color="textSecondary">
+                  {contact.name}
+                </Typography>
+              </div>
+            </Container>
+            <Button color="primary" variant="contained">
+              Apply
+            </Button>
+          </div>
+          <Divider light />
+          <div className="body">
+            <Typography align="justify" color="primary" variant="h6">
+              Overview:
             </Typography>
-          </Grid>
-        </Grid>
-        <Typography
-          align="left"
-          color="primary"
-          display="block"
-          gutterBottom
-          variant="caption"
-        >
-          Publish: {creationDate}
-        </Typography>
-        <Divider variant="middle" />
-        <Typography align="justify" color="textSecondary">
-          Description: {description}
-        </Typography>
-        <Divider variant="middle" />
-        <Typography align="justify" color="textSecondary">
-          Ref.:{textInvitation}
-        </Typography>
-        <Divider variant="middle" />
-        <Typography align="justify" color="primary">
-          Contact Info: {contact.name}
-        </Typography>
-        <Button color="primary" variant="contained">
-          Send CV
-        </Button>
+            <Typography color="textSecondary" gutterBottom variant="body2">
+              {description}
+            </Typography>
+            <Typography align="justify" color="textSecondary">
+              Ref.:{textInvitation}
+            </Typography>
+          </div>
+        </Paper>
       </Container>
     </div>
   );
