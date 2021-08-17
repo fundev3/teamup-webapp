e66d320c00278c71d62a03583087abab3643137a
Rodrigo Baldiviezo <rodrigo.baldiviezo@fundacion-jala.org>
2021-08-17 18:12:51 -0400

    Add constants files

---
+++ src/constants/images.js
@@ -0,0 +1,5 @@
+const CONTAINER = "images";
+const DIR = "webapp";
+const BASE_URL = `${process.env.REACT_APP_STORAGE_URL}/${CONTAINER}/${DIR}`;
+
+export const logoHome = `${BASE_URL}/logo-teamup2.594bdda4.png`;
