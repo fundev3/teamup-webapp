86d1e3cf638df29f80bddfe1ca17419eb2731c1f
Gustavo Rojas Valdivia <Gustavo.Rojas@fundacion-jala.org>
2021-08-26 11:20:44 -0400

    Post Request

--- src/components/projects/ProjectsAPI.js
+++ src/components/projects/ProjectsAPI.js
@@ -85,7 +85,6 @@ export async function postPostulation(postulation) {
   try {
     const { data } = await axios.post(
       `${API_HOST}api/${API_VERSION}/postulation`,
-      // "https://fa-tuapi-resumes-dev-bra.azurewebsites.net/postulations",
       postulation
     );
 
