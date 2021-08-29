548f97bb3ba389a92d99d75a0acc98a785ca155f
Alex Flores <alex.flores@fundacion-jala.com>
2021-08-28 21:54:53 -0400

    some change

--- src/components/projects/ProjectsAPI.js
+++ src/components/projects/ProjectsAPI.js
@@ -84,7 +84,7 @@ export async function getProjectBySkill(skill) {
 export async function postPostulation(postulation) {
   try {
     const { data } = await axios.post(
-      `${API_HOST}api/${API_VERSION}/postulation`,
+      `${API_HOST}/api/${API_VERSION}/postulations`,
       postulation
     );
 
