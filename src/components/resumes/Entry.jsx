57ad846ed358ae28079cd4f6ab453812655bfade
ela-ocana-jalafund <Ela.Ocana@fundacion-jala.org>
2021-08-29 05:47:10 -0400

    refactor UI create resume

--- src/components/resumes/Entry.jsx
+++ src/components/resumes/Entry.jsx
@@ -1,16 +1,50 @@
+import ArrowBackIos from "@material-ui/icons/ArrowBackIos";
 import { entry as entryValidations } from "./helpers/validations";
+import { makeStyles } from "@material-ui/core/styles";
 import { postResume } from "./ResumesAPI.js";
 import { useFormik } from "formik";
-import { useHistory } from "react-router-dom";
-import { Button, TextField } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  Container,
+  Paper,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { Link, useHistory } from "react-router-dom";
 import "./Entry.css";
 
+const useStyles = makeStyles({
+  cancel: {
+    margin: "10px",
+    padding: "8px 22px",
+  },
+  cardTitle: {
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+  },
+  create: {
+    margin: "10px",
+    padding: "8px 22px",
+  },
+  field: {
+    width: "100%",
+  },
+  head: {
+    color: "#ED8749",
+    fontSize: "2.2rem",
+    fontWeight: "bold",
+    margin: "40px 0px 40px 25px",
+  },
+});
+
 function Entry() {
+  const classes = useStyles();
   const history = useHistory();
   const formik = useFormik({
     initialValues: {
+      address: "",
       birthdate: "",
-      direction: "",
       email: "",
       firstName: "",
       lastName: "",
@@ -18,155 +52,242 @@ function Entry() {
       summary: "",
     },
     onSubmit: async (values) => {
-      await postResume(values);
+      const resume = {
+        contact: {
+          address: values.address,
+          email: values.email,
+          phone: values.phone,
+        },
+        creationDate: new Date().toDateString(),
+        lastUpdate: new Date().toDateString(),
+        person: {
+          birthdate: values.birthdate,
+          firstName: values.firstName,
+          lastName: values.lastName,
+          picture: null,
+        },
+        skills: {},
+        summary: values.summary,
+        title: values.firstName + " " + values.lastName,
+      };
+      console.log(resume);
+      await postResume(resume);
       history.push("/");
     },
     validationSchema: entryValidations(),
   });
 
   return (
-    <div className="container-creation-resume">
-      <div className="container-left">
-        <h1>Hello, there!</h1>
-        <h2 id="subtitle">Let's build your profile</h2>
-      </div>
-      <form className="container-right" onSubmit={formik.handleSubmit}>
-        <div className="label">
-          <h3>Name</h3>
-          <TextField
-            error={formik.touched.firstName && formik.errors.firstName}
-            helperText={
-              formik.touched.firstName && formik.errors.firstName
-                ? formik.errors.firstName
-                : ""
-            }
-            id="firstName"
-            name="firstName"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            placeholder="Your name"
-            type="text"
-            value={formik.values.firstName}
-          />
-        </div>
-        <div className="label">
-          <h3>Last Name</h3>
-          <TextField
-            error={formik.touched.lastName && formik.errors.lastName}
-            helperText={
-              formik.touched.lastName && formik.errors.lastName
-                ? formik.errors.lastName
-                : ""
-            }
-            id="lastName"
-            name="lastName"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            placeholder="Your last name"
-            type="text"
-            value={formik.values.lastName}
-          />
-        </div>
-        <div className="label">
-          <h3>Date of birth</h3>
-          <TextField
-            error={formik.touched.birthdate && formik.errors.birthdate}
-            helperText={
-              formik.touched.birthdate && formik.errors.birthdate
-                ? formik.errors.birthdate
-                : ""
-            }
-            id="birthdate"
-            name="birthdate"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            placeholder="dd/mm/yyyy"
-            type="text"
-            value={formik.values.birthdate}
-          />
-        </div>
-        <div className="label">
-          <h3>Address</h3>
-          <TextField
-            error={formik.touched.direction && formik.errors.direction}
-            helperText={
-              formik.touched.direction && formik.errors.direction
-                ? formik.errors.direction
-                : ""
-            }
-            id="direction"
-            name="direction"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            placeholder="Your Address"
-            type="text"
-            value={formik.values.direction}
-          />
-        </div>
-        <div className="label">
-          <h3>Email Address</h3>
-          <TextField
-            error={formik.touched.email && formik.errors.email}
-            helperText={
-              formik.touched.email && formik.errors.email
-                ? formik.errors.email
-                : ""
-            }
-            id="email"
-            name="email"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            placeholder="Your email address"
-            type="email"
-            value={formik.values.email}
-          />
-        </div>
-        <div className="label">
-          <h3>Phone Number</h3>
-          <TextField
-            error={formik.touched.phone && formik.errors.phone}
-            helperText={
-              formik.touched.phone && formik.errors.phone
-                ? formik.errors.phone
-                : ""
-            }
-            id="phone"
-            name="phone"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            placeholder="Your Phone Number"
-            type="number"
-            value={formik.values.phone}
-          />
-        </div>
-        <div className="label">
-          <h3>Add Bio</h3>
-          <TextField
-            error={formik.touched.summary && formik.errors.summary}
-            helperText={
-              formik.touched.summary && formik.errors.summary
-                ? formik.errors.summary
-                : ""
-            }
-            id="summary"
-            maxRows={4}
-            multiline
-            name="summary"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            placeholder="Enter a brief description of you. 
-            Describe your professional skills and experience."
-            value={formik.values.summary}
-            variant="outlined"
-          />
-        </div>
-        <div className="bottom">
-          <Button color="primary" type="submit" variant="contained">
-            Submit
-          </Button>
-        </div>
-      </form>
-    </div>
+    <>
+      <Container className="create-resume-container">
+        <Link className="back-resumes" to="/resumes">
+          <ArrowBackIos></ArrowBackIos>
+          Back
+        </Link>
+        <Typography className={classes.head} gutterBottom variant="h3">
+          Let's get to know you!
+        </Typography>
+        <form autoComplete="off" noValidate onSubmit={formik.handleSubmit}>
+          <Box display="flex" justifyContent="space-between" mb={1}>
+            <Paper className="paper-entry" elevation={1}>
+              <div className="entry-content">
+                <Typography
+                  align="justify"
+                  className={classes.cardTitle}
+                  color="primary"
+                  gutterBottom
+                  variant="h6"
+                >
+                  Personal Information
+                </Typography>
+              </div>
+              <div className="u-mb-1">
+                <Typography align="justify" gutterBottom>
+                  Name
+                </Typography>
+                <TextField
+                  className={classes.field}
+                  error={formik.touched.firstName && formik.errors.firstName}
+                  helperText={
+                    formik.touched.firstName && formik.errors.firstName
+                      ? formik.errors.firstName
+                      : ""
+                  }
+                  id="firstName"
+                  name="firstName"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  placeholder="Your name"
+                  type="text"
+                  value={formik.values.firstName}
+                  variant="outlined"
+                />
+              </div>
+              <div className="u-mb-1">
+                <Typography align="justify" gutterBottom>
+                  Last Name
+                </Typography>
+                <TextField
+                  className={classes.field}
+                  error={formik.touched.lastName && formik.errors.lastName}
+                  helperText={
+                    formik.touched.lastName && formik.errors.lastName
+                      ? formik.errors.lastName
+                      : ""
+                  }
+                  id="lastName"
+                  name="lastName"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  placeholder="Your last name"
+                  type="text"
+                  value={formik.values.lastName}
+                  variant="outlined"
+                />
+              </div>
+              <div className="u-mb-1">
+                <Typography align="justify" gutterBottom>
+                  Date of birth
+                </Typography>
+                <TextField
+                  className={classes.field}
+                  error={formik.touched.birthdate && formik.errors.birthdate}
+                  helperText={
+                    formik.touched.birthdate && formik.errors.birthdate
+                      ? formik.errors.birthdate
+                      : ""
+                  }
+                  id="birthdate"
+                  name="birthdate"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  placeholder="dd/mm/yyyy"
+                  type="text"
+                  value={formik.values.birthdate}
+                  variant="outlined"
+                />
+              </div>
+              <div className="u-mb-1">
+                <Typography align="justify" gutterBottom>
+                  Add Bio
+                </Typography>
+                <TextField
+                  className={classes.field}
+                  error={formik.touched.summary && formik.errors.summary}
+                  helperText={
+                    formik.touched.summary && formik.errors.summary
+                      ? formik.errors.summary
+                      : ""
+                  }
+                  id="summary"
+                  maxRows={5}
+                  multiline
+                  name="summary"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  placeholder="Enter a brief description of you. 
+                  Describe your professional skills and experience."
+                  value={formik.values.summary}
+                  variant="outlined"
+                />
+              </div>
+            </Paper>
+            <Paper className="paper-entry" elevation={1}>
+              <div className="entry-content">
+                <Typography
+                  align="justify"
+                  className={classes.cardTitle}
+                  color="primary"
+                  gutterBottom
+                  variant="h5"
+                >
+                  Contact Information
+                </Typography>
+              </div>
+              <div className="u-mb-1">
+                <Typography align="justify" gutterBottom>
+                  Address
+                </Typography>
+                <TextField
+                  className={classes.field}
+                  error={formik.touched.address && formik.errors.address}
+                  helperText={
+                    formik.touched.address && formik.errors.address
+                      ? formik.errors.address
+                      : ""
+                  }
+                  id="address"
+                  name="address"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  placeholder="Your Address"
+                  type="text"
+                  value={formik.values.address}
+                  variant="outlined"
+                />
+              </div>
+              <div className="u-mb-1">
+                <Typography align="justify" gutterBottom>
+                  Email Address
+                </Typography>
+                <TextField
+                  className={classes.field}
+                  error={formik.touched.email && formik.errors.email}
+                  helperText={
+                    formik.touched.email && formik.errors.email
+                      ? formik.errors.email
+                      : ""
+                  }
+                  id="email"
+                  name="email"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  placeholder="Your email address"
+                  type="email"
+                  value={formik.values.email}
+                  variant="outlined"
+                />
+              </div>
+              <div className="u-mb-1">
+                <Typography align="justify" gutterBottom>
+                  Phone
+                </Typography>
+                <TextField
+                  className={classes.field}
+                  error={formik.touched.phone && formik.errors.phone}
+                  helperText={
+                    formik.touched.phone && formik.errors.phone
+                      ? formik.errors.phone
+                      : ""
+                  }
+                  id="phone"
+                  name="phone"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  placeholder="Your Phone Number"
+                  type="number"
+                  value={formik.values.phone}
+                  variant="outlined"
+                />
+              </div>
+            </Paper>
+          </Box>
+          <Box display="flex" justifyContent="flex-end">
+            <Button className={classes.cancel} variant="contained">
+              Cancel
+            </Button>
+            <Button
+              className={classes.create}
+              color="primary"
+              type="submit"
+              variant="contained"
+            >
+              Create
+            </Button>
+          </Box>
+        </form>
+      </Container>
+    </>
   );
 }
 
